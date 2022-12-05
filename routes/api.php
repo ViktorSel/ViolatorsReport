aa28b9f959892aba21da27c603ed1cd21c215782
VikSel <v.selednikov@iceberg.ru>
2022-12-05 20:14:21 +0300

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,17 @@ use Ast\ViolatorsReport\Http\Controllers\ViolatorsReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'prefix' => 'violators-report',
+    'prefix'    => 'api/ui',
+    /* Для оставления связи со старым проектом */
     'middleware' => ['ui'],
-], function (){
-    Route::post('export', [ViolatorsReportController::class, 'export']);
-    Route::post('get-sections',[ViolatorsReportController::class, 'get_sections']);
+],function(){
+
+    Route::group([
+        'prefix' => 'violators-report',
+    ], function (){
+        Route::post('export', [ViolatorsReportController::class, 'export']);
+        Route::post('get-sections',[ViolatorsReportController::class, 'get_sections']);
+    });
+
 });
+
